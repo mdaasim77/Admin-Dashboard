e929d04572fce5152f8cd29b4384d3f4d5c0b5cd
mdaasim77 <khanaasim9910@gmail.com>
2025-12-29 14:21:47 +0530

    working on sidebar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,7 +2,15 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex bg-gray-100">
       {/* Sidebar */}
-      <aside className="w-64 bg-gray-900 text-white">Sidebar</aside>
+      <aside className="w-64 bg-gray-900 text-white flex flex-col">
+        <div className="px-6 py-5 border-b">Logo</div>
+        <nav className="flex-1 px-6 py-5 space-y-2">
+          <div>Dashboard</div>
+          <div>Users</div>
+          <div>Settings</div>
+        </nav>
+        <footer className="px-6 py-5 border-t">Footer</footer>
+      </aside>
 
       {/* Main section */}
       <div className="flex-1 flex flex-col">
