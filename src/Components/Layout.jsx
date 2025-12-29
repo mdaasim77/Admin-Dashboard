3ee4d77bee17f629f219af2fdac73371cbcdf3b0
mdaasim77 <khanaasim9910@gmail.com>
2025-12-29 23:21:54 +0530

    clicking work on layout

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
+
 export default function Layout({ children }) {
+  const [currentpage, setCurrentpage] = useState("dashboard");
+
   return (
     <div className="min-h-screen flex bg-gray-100">
       {/* Sidebar */}
       <aside className="w-64 bg-gray-900 text-white flex flex-col">
         <div className="px-6 py-5 border-b">Logo</div>
         <nav className="flex-1 flex flex-col px-6 py-4 gap-2">
-          <a href="#" className="px-3 py-2 rounded block">
+          <a
+            href="#"
+            onClick={() => setCurrentpage("dashboard")}
+            className={`block px-3 py-2 rounded  ${
+              currentpage === "dashboard" ? "font-semibold" : ""
+            }`}
+          >
             Dashboard
           </a>
-          <a href="#" className="px-3 py-2 rounded block">
+          <a
+            href="#"
+            onClick={() => setCurrentpage("user")}
+            className={`block px-3 py-2 rounded ${
+              currentpage === "user" ? "font-semibold" : ""
+            }`}
+          >
             Users
           </a>
-          <a href="#" className="px-3 py-2 rounded block">
+          <a
+            href="#"
+            onClick={() => setCurrentpage("settings")}
+            className={`px-3 py-2 rounded block ${
+              currentpage === "settings" ? "font-semibold" : ""
+            }`}
+          >
             Settings
           </a>
         </nav>
@@ -24,7 +46,11 @@ export default function Layout({ children }) {
         <header className="h-16 bg-white border-b">Navbar</header>
 
         {/* Page Content */}
-        <main className="flex-1 p-6">{children}</main>
+        <main className="flex-1 p-6">
+          {currentpage === "dashboard" && <h1>DashBoard Page</h1>}
+          {currentpage === "user" && <h1>User Page</h1>}
+          {currentpage === "settings" && <h1>Setting Page</h1>}
+        </main>
       </div>
     </div>
   );
